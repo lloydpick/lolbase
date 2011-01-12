e95d6a33100626511bd8c425be49743651d9061e
Lloyd Pick <lloydpick@gmail.com>
2011-01-12 18:45:08 +0000

    ability to get a page

--- lib/lolbase/api/client.rb
+++ lib/lolbase/api/client.rb
@@ -11,7 +11,7 @@ module Lolbase
       def self.included(base)
         base.class_eval do
           @@base_url = 'lolbase.net'.freeze
-          @@search_url = '/search'.freeze
+          @@search_url = 'search'.freeze
           @@max_connection_tries = 10.freeze
           @@cache_directory_path = 'cache/'
           @@failed_cache_timeout = (60*60*24)
@@ -23,6 +23,35 @@ module Lolbase
           cattr_reader :cache_directory_path
           cattr_reader :failed_cache_timeout
           cattr_reader :cache_failed_requests
+        end
+      end
+
+      def base_url(options = {})
+        # Non-secure only
+        str = 'http://'
+        str += @@base_url
+      end
+
+      protected
+
+      # Given a url and a hash of query parameters, fetches a file from the site
+      def get_file(url, options = {})
+        client = LolbaseClient
+        client.base_uri(self.base_url(options) + "/")
+
+        if options[:caching]
+          client.cache :store => 'file', :timeout => @cache_timeout, :location => @@cache_directory_path
+        else
+          # Default setting
+          client.cache :store => 'memory', :timeout => 60
+        end
+
+        begin
+          client.get(url).response.body
+        rescue Timeout::Error => e
+          raise Lolbase::Exceptions::NetworkTimeout.new('Timed out - Timeout::Error Exception')
+        rescue SocketError, Net::HTTPExceptions => e
+          raise Lolbase::Exceptions::ServerDoesNotExist.new('Specified server at ' + url + ' does not exist.')
         end
       end
 
---
+++ lib/lolbase/exceptions.rb
@@ -0,0 +1,55 @@
+module Lolbase
+  module Exceptions
+    def self.raise_me(code, options = {})
+      case code
+        when "noCharacter"
+          raise CharacterNotFound.new(options[:character_name])
+        else
+          raise StandardError.new("The site returned an error: #{code.to_s}")
+      end
+    end
+
+    class InvalidXML < StandardError
+    end
+
+    class EmptyPage < StandardError
+    end
+
+    class ServerDoesNotExist < StandardError
+      def initialize(string)
+        super "Server at '#{string}' did not respond."
+      end
+    end
+
+    class CharacterNameNotSet < StandardError
+      def initialize
+        super "Character name not set in options or API constructor."
+      end
+    end
+
+    # Search (fold)
+    class SearchError < StandardError
+    end
+
+    class NoSearchString < SearchError
+      def initialize
+        super "No search string specified or string was empty."
+      end
+    end
+
+    class ElementNotFoundError < StandardError
+    end
+
+    class CharacterNotFound < ElementNotFoundError
+      def initialize(string)
+        super "Character not found with name '#{string}'."
+      end
+    end
+
+    class NetworkTimeout < StandardError
+      def initialize(string)
+        super "Network timeout '#{string}'."
+      end
+    end
+  end
+end
