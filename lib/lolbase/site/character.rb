0da80f43c2404768a0d27a925942201f295dc272
Lloyd Pick <lloydpick@gmail.com>
2011-01-15 18:13:50 +0000

    actual stats woo!

--- lib/lolbase/site/character.rb
+++ lib/lolbase/site/character.rb
@@ -19,6 +19,20 @@ module Lolbase
         attr_reader :region
         attr_reader :last_update
 
+        attr_reader :normal_wins
+        attr_reader :normal_wins_real
+        attr_reader :normal_losses
+        attr_reader :normal_losses_real
+        attr_reader :normal_leaves
+        attr_reader :normal_leaves_real
+
+        attr_reader :ranked_wins
+        attr_reader :ranked_wins_real
+        attr_reader :ranked_losses
+        attr_reader :ranked_losses_real
+        attr_reader :ranked_leaves
+        attr_reader :ranked_leaves_real
+
         alias_method :to_s, :name
         alias_method :to_i, :level
         alias_method :last_modified, :last_update
@@ -32,6 +46,20 @@ module Lolbase
           @url         = elem.xpath('td[2]/a').map { |link| link['href'].split(@api.base_url)[1] }.to_s
           @region      = @api.region
           @last_update = elem.xpath('td[10]').text
+
+          @normal_wins        = elem.xpath('td[4]').text.scan(/(\d*)\/\d* \(\d*\/\d*\)/).to_s.to_i
+          @normal_wins_real   = elem.xpath('td[4]').text.scan(/\d*\/\d* \((\d*)\/\d*\)/).to_s.to_i
+          @normal_losses      = elem.xpath('td[5]').text.scan(/(\d*)\/\d* \(\d*\/\d*\)/).to_s.to_i
+          @normal_losses_real = elem.xpath('td[5]').text.scan(/\d*\/\d* \((\d*)\/\d*\)/).to_s.to_i
+          @normal_leaves      = elem.xpath('td[6]').text.scan(/(\d*)\/\d* \(\d*\/\d*\)/).to_s.to_i
+          @normal_leaves_real = elem.xpath('td[6]').text.scan(/\d*\/\d* \((\d*)\/\d*\)/).to_s.to_i
+
+          @ranked_wins        = elem.xpath('td[4]').text.scan(/\d*\/(\d*) \(\d*\/\d*\)/).to_s.to_i
+          @ranked_wins_real   = elem.xpath('td[4]').text.scan(/\d*\/\d* \(\d*\/(\d*)\)/).to_s.to_i
+          @ranked_losses      = elem.xpath('td[5]').text.scan(/\d*\/(\d*) \(\d*\/\d*\)/).to_s.to_i
+          @ranked_losses_real = elem.xpath('td[5]').text.scan(/\d*\/\d* \(\d*\/(\d*)\)/).to_s.to_i
+          @ranked_leaves      = elem.xpath('td[6]').text.scan(/\d*\/(\d*) \(\d*\/\d*\)/).to_s.to_i
+          @ranked_leaves_real = elem.xpath('td[6]').text.scan(/\d*\/\d* \(\d*\/(\d*)\)/).to_s.to_i
 
         end
 
--- spec/lolbase/site/character_spec.rb
+++ spec/lolbase/site/character_spec.rb
@@ -4,22 +4,38 @@ module Lolbase::Site::Character
   describe Base do
     subject { Base.new(fake_element, Lolbase::API.new()) }
 
-    its(:id)     { should eql('EU19915678') }
-    its(:name)   { should eql('Limeh') }
+    # Basics
+    its(:id)     { should eql('EU19265646') }
+    its(:name)   { should eql('Dux0r') }
     its(:level)  { should eql(30) }
     its(:region) { should eql('eu') }
-    its(:url)    { should eql("/eu/Limeh") }
+    its(:url)    { should eql("/eu/Dux0r") }
+
+    # Stats
+    its(:normal_wins) { should eql(101) }
+    its(:normal_wins_real) { should eql(354) }
+    its(:normal_losses) { should eql(88) }
+    its(:normal_losses_real) { should eql(243) }
+    its(:normal_leaves) { should eql(0) }
+    its(:normal_leaves_real) { should eql(1) }
+
+    its(:ranked_wins) { should eql(6) }
+    its(:ranked_wins_real) { should eql(18) }
+    its(:ranked_losses) { should eql(7) }
+    its(:ranked_losses_real) { should eql(9) }
+    its(:ranked_leaves) { should eql(0) }
+    its(:ranked_leaves_real) { should eql(1) }
 
     # Aliased methods
-    its(:to_s)          { should eql('Limeh') }
+    its(:to_s)          { should eql('Dux0r') }
     its(:to_i)          { should eql(30) }
     its(:last_modified) { should_not be_nil }
 
     protected
 
     def fake_element
-      html = Nokogiri::HTML(read_fixture('search_limeh'))
-      html.xpath('//div[@class="table"]/table/tbody/tr')[3]
+      html = Nokogiri::HTML(read_fixture('search_dux0r'))
+      html.xpath('//div[@class="table"]/table/tbody/tr')
     end
   end
 end
